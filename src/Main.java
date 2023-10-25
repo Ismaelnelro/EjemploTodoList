6d8c2fe51609c37d1e2252333dc59e8e1f481f24
ismaelnelro <ismaelnelsonrosas@hotmail.com>
2023-10-25 18:10:00 -0300

    Se actualizo el Main con la posibilidad de agregar una tercera tarea

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,17 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         List<String> tareas = new ArrayList<>();
 
-        System.out.println("Agrege primer tarea");
+        System.out.println("Agregar primer tarea");
         String tarea1 = sc.next();
         tareas.add(tarea1);
 
-        System.out.println("Agrege segunda tarea");
+        System.out.println("Agregar segunda tarea");
         String tarea2 = sc.next();
         tareas.add(tarea2);
+
+
+        System.out.println("Agregar tercera tarea");
+        String tarea3 = sc.next();
+        tareas.add(tarea3);
     }
 }
