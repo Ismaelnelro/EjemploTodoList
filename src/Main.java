cf97ecd98a3e228a954ba65b95d3ce92b6550da1
ismaelnelro <ismaelnelsonrosas@hotmail.com>
2023-10-25 18:48:55 -0300

    removiendo codigo

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        List<String> tareas = new ArrayList<>();
-
-        System.out.println("Agregar primer tarea");
-        String tarea1 = sc.next();
-        tareas.add(tarea1);
-
-        System.out.println("Agregar segunda tarea");
-        String tarea2 = sc.next();
-        tareas.add(tarea2);
 
     }
 }
